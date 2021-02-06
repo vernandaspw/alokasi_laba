642046551793b2fee4ae1682a5f6576e853d42a8
vernandaspw <vernandaspw@gmail.com>
2021-02-06 13:38:17 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,8 +5,8 @@
 // $database   =   "db_alokasi_laba";
 
 $host       =   "us-cdbr-east-03.cleardb.com";
-$user       =   "b969e011cf9d21";
-$password   =   "f2ffe39b";
-$database   =   "heroku_c729ddfd489df82";
+$user       =   "b0ed77d965f3c4";
+$password   =   "d813a978";
+$database   =   "heroku_6964a60fb425567";
 
 $conn = mysqli_connect($host, $user, $password, $database);
